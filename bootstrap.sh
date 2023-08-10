f64d3cb2ab070e679121733f3300b453b52a3766
Tihomir Bosnjak <tihomir.bosnjak@allianz.de>
2023-08-10 21:11:41 +0200

    unset ZSH

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,7 +17,7 @@ sudo apt install zsh -y
 sudo usermod -s /usr/bin/zsh $USER
 
 # Install Oh My Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+ZSH= sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install powerlevel10k 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
