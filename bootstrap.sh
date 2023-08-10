8117910f03f66674b4f808b100857b6196de33fc
Tihomir Bosnjak <tihomir.bosnjak@allianz.de>
2023-08-10 21:46:10 +0200

    fix the second sed

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,14 +2,14 @@
 
 # Disable annoying message about kernel upgrade
 sudo sed -i "s/#\$nrconf{kernelhints} = -1;/\$nrconf{kernelhints} = -1;/g" /etc/needrestart/needrestart.conf
-sudo sed "/#\$nrconf{restart} = 'i';/s/.*/\$nrconf{restart} = 'a';/" /etc/needrestart/needrestart.conf
+sudo sed -i "/#\$nrconf{restart} = 'i';/s/.*/\$nrconf{restart} = 'a';/" /etc/needrestart/needrestart.conf
 
 # Initial updating of the software
 sudo apt update
 sudo apt upgrade -y
 
 # Installing of some usual utilities
-sudo apt install -y curl git vim wget ca-certificates apt-transport-https 
+sudo apt install -y curl git vim wget ca-certificates apt-transport-https
 
 # Install ZSH
 sudo apt install zsh -y
@@ -20,7 +20,7 @@ sudo usermod -s /usr/bin/zsh $USER
 # Install Oh My Zsh
 ZSH= sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# Install powerlevel10k 
+# Install powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # Install zsh-syntax-highlighting
